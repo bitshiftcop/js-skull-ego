dd0ded2dfd82fcf323b013173af384cf1d02277d
Bart Claessens <bart.cla@gmail.com>
2015-04-13 22:12:44 +0200

    Gui for colors, more animations, ego position update to screenspace

--- app/scripts/application.js
+++ app/scripts/application.js
@@ -14,12 +14,20 @@ Application.prototype = {
 
     // create scene
     this.scene = new Scene();
-
+    this.scene.delegate = this;
 
     // add mouse move listener
     $('body').mousemove( function() {
       this.scene.mousemove( event );
     }.bind( this ) );
 
+    // click handler
+    $('#info-box').click( function() {
+      this.scene.next();
+    }.bind( this ) );
   },
+
+  onSceneEgoPositionUpdate: function(position) {
+    TweenMax.to($('#info-box'), 0.1, {left:position.x, top:position.y});
+  }
 };
--- app/scripts/scene.js
+++ app/scripts/scene.js
@@ -4,6 +4,13 @@ function Scene() {
 
   // consts
   this.CLEAR_COLOR = 0x000000;
+  this.AMBI_LIGHT_COLOR = 0xffffff;
+  this.SKULL_FILM_COLOR = 0x41008c;
+  this.SKULL_WIRE_COLOR = 0x00a9ff;
+  this.EGO_COLOR = 0xd2ff00;
+
+  // delegate
+  this.delegate = null;
 
   // camera, scene, renderer
   this.camera = null;
@@ -19,9 +26,13 @@ function Scene() {
   this.skullWireMesh = null;
   this.ego = null;
 
-  // create scene, start cycle
+  // create scene, start animation cycle
   this.createScene( function(){
-    this.animate();
+
+    // init animation ticker
+    TweenMax.ticker.fps(60);
+    TweenMax.ticker.addEventListener( 'tick', this.animate.bind( this ) );
+
   }.bind( this ) );
 }
 
@@ -36,8 +47,11 @@ Scene.prototype = {
       egoGeometry,
       egoMaterial,
       skullUrls = [ 'obj/skull.obj', 'obj/skull.mtl' ],
-      skullLoader;
-      //gui;
+      skullLoader,
+      guiParams,
+      lightsFolder,
+      materialFolder,
+      gui;
 
 
     // create camera
@@ -51,12 +65,12 @@ Scene.prototype = {
 
 
     // create ambi light
-    ambiLight = new THREE.AmbientLight( 0x999999 );
-    this.scene.add( ambiLight );
+    ambiLight = new THREE.AmbientLight( this.AMBI_LIGHT_COLOR );
+    //this.scene.add( ambiLight );
 
 
     // create hemi light
-    hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.25 );
+    hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 1 );
     hemiLight.color.setHSL( 0.6, 1, 0.6 );
     hemiLight.groundColor.setHSL( 0.095, 1, 0.75 );
     hemiLight.position.set( 0, 500, 0 );
@@ -72,13 +86,13 @@ Scene.prototype = {
 
     // skull materials
     skullFilmMaterial = new THREE.MeshLambertMaterial({
-      color: 0x2194ce,
+      color: this.SKULL_FILM_COLOR,
       shading: THREE.SmoothShading,
       transparent:true
     });
 
     skullWireMaterial = new THREE.MeshLambertMaterial({
-      color: 0x2194ce,
+      color: this.SKULL_WIRE_COLOR,
       shading: THREE.SmoothShading,
       transparent:true,
       wireframe: true
@@ -88,7 +102,7 @@ Scene.prototype = {
     // ego geometry & material
     egoGeometry = new THREE.IcosahedronGeometry( 35, 0 );
     egoMaterial = new THREE.MeshLambertMaterial({
-      color: 0xFF9100,
+      color: this.EGO_COLOR,
       shading: THREE.SmoothShading,
       wireframe: true
     });
@@ -100,7 +114,9 @@ Scene.prototype = {
 
       // set skull
       this.skull = obj;
-      this.skull.rotation.y = -0.5;
+      this.skull.scale.set(0.75, 0.75, 0.75);
+      //this.skull.position.setX(-150);
+      this.skull.rotation.y = -0.25;
 
       // traverse skull child objects
       this.skull.traverse(function ( child ) {
@@ -158,27 +174,55 @@ Scene.prototype = {
     this.mouse = new THREE.Vector2();
 
 
-    // // raycaster
-    // this.raycaster = new THREE.Raycaster();
+    // create gui
+    guiParams = {
+      ambiLightColor: ambiLight.color.getHex(),
+      skullFilmMaterialColor: skullFilmMaterial.color.getHex(),
+      skullWireMaterialColor: skullWireMaterial.color.getHex(),
+      egoMaterialColor: egoMaterial.color.getHex()
+    };
 
+    gui = new dat.GUI();
 
-    // // create gui
-    // gui = new dat.GUI();
+    lightsFolder = gui.addFolder( 'Lights' );
 
-    // gui.add({
-    //   dropTrees:function(){
-    //     this.dropTrees(_.random(10, 30));
-    //   }.bind( this )}, 'dropTrees');
+    lightsFolder
+      .add( ambiLight, 'visible' )
+      .name( 'Enable Ambient' );
 
-    // gui.add({
-    //   eraseTrees:function(){
-    //     this.eraseTrees(_.random(3, 10));
-    //   }.bind( this )}, 'eraseTrees');
+    lightsFolder
+      .addColor( guiParams, 'ambiLightColor' )
+      .name( 'Ambient Color' )
+      .onChange(function( color ){
+        ambiLight.color.setHex( color );
+      }.bind( this ) );
 
-    // gui.add({
-    //   toggleCamera:function(){
-    //     this.setCameraSettings(this.activeCameraSetting === this.cameraSettings.god ? this.cameraSettings.person : this.cameraSettings.god );
-    //   }.bind( this )}, 'toggleCamera');
+    //lightsFolder.open();
+
+    materialFolder = gui.addFolder( 'Materials' );
+
+    materialFolder
+      .addColor( guiParams, 'skullFilmMaterialColor' )
+      .name( 'Skull Film Color' )
+      .onChange( function( color ){
+        skullFilmMaterial.color.setHex( color );
+      }.bind( this ) );
+
+    materialFolder
+      .addColor( guiParams, 'skullWireMaterialColor' )
+      .name( 'Skull Wire Color' )
+      .onChange( function( color ){
+        skullWireMaterial.color.setHex( color );
+      }.bind( this ) );
+
+    materialFolder
+      .addColor( guiParams, 'egoMaterialColor' )
+      .name( 'Ego Color' )
+      .onChange( function( color ){
+        egoMaterial.color.setHex( color );
+      }.bind( this ) );
+
+    //materialFolder.open();
   },
 
 
@@ -188,14 +232,74 @@ Scene.prototype = {
 
     this.mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
     this.mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
+    // move camera
+    TweenMax.to(this.camera.position, 1, {
+      x: this.mouse.x * 200,
+      y: this.mouse.y * 200
+    });
+  },
+
+
+  next: function ( ) {
+
+    var newRotation = this.skull.rotation.y + (360 * 5 * Math.PI / 180);
+
+    TweenMax.to(
+      this.skull.rotation,
+      0.65, {
+        y: newRotation,
+        ease:Back.easeOut.config(1)
+      });
+
+    TweenMax.to([
+      this.skullFilmMesh.material,
+      this.skullWireMesh.material ],
+      0.3, {
+        opacity:0.25,
+        ease:Power3.easeIn
+      }
+    );
+
+    TweenMax.to([
+      this.skullFilmMesh.material,
+      this.skullWireMesh.material ],
+      0.5, {
+        delay:0.3,
+        opacity:1,
+        ease: RoughEase.ease.config({
+          template: Power0.easeNone,
+          strength: 3,
+          points: 50,
+          taper: 'none',
+          randomize: true,
+          clamp: true
+        })
+      }
+    );
+
+    TweenMax.to([
+      this.skullFilmMesh.material ],
+      0.5, {
+        delay:1.15,
+        opacity:0.35,
+        ease:Sine.easeInOut
+      }
+    );
+
+    TweenMax.to([
+      this.skullWireMesh.material ],
+      0.5, {
+        delay:1.15,
+        opacity:0.65,
+        ease:Sine.easeInOut
+      }
+    );
   },
 
 
   // animate cycle
   animate: function() {
-    requestAnimationFrame(
-      this.animate.bind( this )
-    );
 
     this.render();
   },
@@ -206,11 +310,18 @@ Scene.prototype = {
 
     this.ego.rotation.x += 0.075;
 
-    this.skullFilmMesh.material.opacity = 0.25 + (Math.abs(this.mouse.x) / 5);
-    this.skullWireMesh.material.opacity = 0.65 + (Math.abs(this.mouse.x) / 5);
+    //this.skullFilmMesh.material.opacity = 0.25 + ( Math.abs( this.mouse.x ) / 5 );
+    //this.skullWireMesh.material.opacity = 0.65 + ( Math.abs( this.mouse.x ) / 5 );
 
     this.camera.lookAt( this.scene.position );
 
-    this.renderer.render(this.scene, this.camera);
+    // notify delegate about ego position
+    if(this.delegate && this.delegate.onSceneEgoPositionUpdate) {
+      this.delegate.onSceneEgoPositionUpdate(
+        THREEx.ObjCoord.cssPosition(this.ego, this.camera, this.renderer)
+      );
+    }
+
+    this.renderer.render( this.scene, this.camera );
   }
 };
